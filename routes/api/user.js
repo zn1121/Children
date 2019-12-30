f88f6d068faed13866fa152c5e565daf76bdbd02
zhangning <zhangning@yunji.ai>
2019-12-30 17:22:29 +0800

    增加对所有接口进行登录权限控制

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,4 +1,6 @@
 const db = require('../../lib/model/db').db;
+var express = require('express');
+var router = express.Router();
 
 exports.user = function (req, res) {
     console.log(req.session)
@@ -24,6 +26,39 @@ exports.user = function (req, res) {
         console.log("未登录")
     }
 }
+exports.users = function (req, res) {
+    db.query('select * from user', (err, result) => {
+        if (err) {
+            res.send({
+                status: 0,
+                info: 'error',
+                message: '数据库错误'
+            })
+        } else {
+            res.send(result);
+            console.log(result)
+        }
+    })
+
+}
+exports.userss = function (req, res) {
+    console.log(req.session)
+    db.query('select * from user', (err, result) => {
+        if (err) {
+            res.send({
+                status: 0,
+                info: 'error',
+                message: '数据库错误'
+            })
+        } else {
+            res.send(result);
+            console.log(result)
+        }
+    })
+
+}
+
+
 exports.login = function (req, res) {
     req.session.userinfo = '李四';//设置session
     res.send("登陆成功！");
